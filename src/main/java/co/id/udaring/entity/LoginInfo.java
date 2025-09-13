eb32c4708714d797138b8ae90ef4ffe1d05fbb92
Candi Mahendra <abdumaadjidcandi@gmail.com>
2025-09-14 01:27:59 +0700

    Use jwt as refresh token

--- src/main/java/co/id/udaring/entity/LoginInfo.java
+++ src/main/java/co/id/udaring/entity/LoginInfo.java
@@ -1,29 +1,47 @@
 package co.id.udaring.entity;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
 @Getter
 @Entity
 public class LoginInfo {
     @Id
+    @NotNull
     private UUID id; // either tenant table key or user id
-    private String refreshToken;
+    @NotNull
+    private byte[] refreshToken;
+    @NotNull
     private LocalDateTime expiresAt;
 
     protected LoginInfo() {}
 
     private LoginInfo(UUID id, String refreshToken, LocalDateTime expiresAt) {
         this.id = id;
-        this.refreshToken = refreshToken;
+        this.refreshToken = refreshToken.getBytes();
         this.expiresAt = expiresAt;
     }
 
     public static LoginInfo generateRefreshToken(UUID id) {
-        return new LoginInfo(id, UUID.randomUUID().toString(), LocalDateTime.now().plusDays(3));
+        final var expiresAt = Instant.now().plus(3, ChronoUnit.DAYS);
+        final var refreshToken = JWT.create()
+                .withSubject(id.toString())
+                .withExpiresAt(expiresAt)
+                .sign(Algorithm.HMAC256("secret")); // todo change me to properties
+        return new LoginInfo(id, refreshToken, LocalDateTime.ofInstant(expiresAt, ZoneId.systemDefault()));
+    }
+
+    public String getRefreshToken() {
+        return new String(refreshToken);
     }
 }
